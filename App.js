630c45d0b107e35df260fb3c4c83d1b4e5b58a4b
KevinRaleie-dev <kkevraleie@gmail.com>
2020-10-30 17:45:01 +0200

    added a login screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Register from './screens/Register';
+import Login from './screens/Login';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Register' component={Register}/>
+        <Stack.Screen name='Login' component={Login}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Container from '../components/Container';
+
+const Login = () => {
+    return (
+        <Container>
+            <Text>Login screen</Text>
+        </Container>
+        
+    )
+}
+
+export default Login
